20cdc798ef419ff84b90c6ab1efd02bbaac3a8fe
Amal Joe R S <amaljoe88@gmail.com>
2021-09-23 20:19:02 +0530

    bankers setup

--- bankers algo/bankers.c
+++ bankers algo/bankers.c
@@ -5,16 +5,88 @@
 int available[MAX_REQUEST];
 int max[MAX_PROCESS][MAX_REQUEST];
 int alloc[MAX_PROCESS][MAX_REQUEST];
+int need[MAX_PROCESS][MAX_REQUEST];
 // n - no of process, m - no of resources
 int n, m;
 
 void safety();
 void resource_req(int[]);
+void display();
 
 int main()
 {
-    n = 1;
-    printf("Hello world");
-    n = n + 1;
-    printf("%d\n", n);
+    printf("Enter the no of processes:\n");
+    scanf("%d", &n);
+    printf("Enter the no of resources:\n");
+    scanf("%d", &m);
+    printf("Enter available resources (a1 a2 a3...):\n");
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d", &available[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter current allocation for resource p%d (c1 c2 c3...):\n", i);
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &alloc[i][j]);
+        }
+        printf("Enter maximum allocation for resource p%d (m1 m2 m3...):\n", i);
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &max[i][j]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            need[i][j] = max[i][j] - alloc[i][j];
+            if (need[i][j] < 0)
+            {
+                printf("Invalid inputs");
+                return 1;
+            }
+        }
+    }
+    display();
+    return 0;
 }
+
+void display()
+{
+    printf("Process\tAllocated\tMax\tNeed\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("p%d\t", i);
+        for (int j = 0; j < m; j++)
+        {
+            printf("%d ", alloc[i][j]);
+        }
+        printf("\t\t");
+        for (int j = 0; j < m; j++)
+        {
+            printf("%d ", max[i][j]);
+        }
+        printf("\t");
+        for (int j = 0; j < m; j++)
+        {
+            printf("%d ", need[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+/*
+4
+3
+1 2 3
+1 2 3
+2 3 4
+1 2 3
+2 3 4
+1 2 3
+2 3 4
+1 2 3
+2 3 4
+*/
